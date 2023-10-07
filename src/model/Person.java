4525d2929dbedbebebda84ea74592931c8a1a226
VietAnh <havietanh5971@gmail.com>
2023-10-07 09:34:10 +0700

    Initial commit

---
+++ src/model/Person.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.Date;
+
+public class Person {
+    protected static int Id;
+    protected String name;
+    protected Date dateOfBirth;
+    protected String adrress;
+    protected double height;
+    protected double weight;
+
+
+
+
+}
